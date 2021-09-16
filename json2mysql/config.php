813bb1b3f58006819e5e78aa5a6ad9a3a7fc207e
Valerzen88 <valeri.balachnin@gmail.com>
2021-09-16 21:43:53 +0200

    first commit

---
+++ json2mysql/config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Licensed under Creative Commons 3.0 Attribution
+ * Copyright Adam Wulf 2013
+ */
+
+define("ROOT", dirname(__FILE__) . "/");
+const DATABASE_HOST = "127.0.0.1";
+const DATABASE_NAME = "kinozone";
+const DATABASE_USER = "kinozone_usr";
+const DATABASE_PASS = "root";
+const JSONTOMYSQL_LOCKED = false;
+
