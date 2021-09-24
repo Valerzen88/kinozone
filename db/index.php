156bc300fee6d2822ff78ccf3bec515223ba11d1
Valerzen88 <valeri.balachnin@gmail.com>
2021-09-24 15:54:02 +0200

    add flag for startId in sql query

--- db/index.php
+++ db/index.php
@@ -9,7 +9,7 @@
  * convert json to mysql statements
  * add information to mysql db
  */
-require '../../../vendor/autoload.php';
+require 'vendor/autoload.php';
 include('json2mysql/config.php');
 include('json2mysql/include.classloader.php');
 
@@ -29,7 +29,7 @@ if(isset($_GET["addNewFilmsJob"])) {
     $lastId = 290;
     $time_pre = microtime(true);
     for($i=0;$i<$_GET["addNewFilmsJob"];$i++) {
-        $sql = "SELECT MAX(kinopoiskId) FROM films";
+        $sql = "SELECT MAX(kinopoiskId) FROM films where kinopoiskId<1100000";
         $result = mysqli_query($conn, $sql);
         if ($result) {
             while ($row = mysqli_fetch_row($result)) {
@@ -37,12 +37,14 @@ if(isset($_GET["addNewFilmsJob"])) {
             }
             mysqli_free_result($result);
         }
-        $amount_ids = 10000;
+        $amount_ids = 1000;
+        if(isset($_GET["startId"])){$lastId=$_GET["startId"];}
         echo "+++starting job for films data with lastId=" . $lastId . ";step=".$amount_ids.";+++<br>";
-        $lastId = addFilms($lastId, $amount_ids, $db, $conn);
+        $newlastId = addFilms($lastId, $amount_ids, $db, $conn);
         $time_post = microtime(true);
         $exec_time = $time_post - $time_pre;
-        echo "<br>+++end job with " . $lastId . " new lines in ".sprintf('%f', $exec_time)." +++<br>";
+        if(isset($_GET["startId"])){$lastId=$_GET["startId"]+$amount_ids;}
+        echo "<br>+++end job with " . $newlastId . " new lines in ".sprintf('%f', $exec_time)." +++<br>";
     }
 }
 
@@ -92,7 +94,7 @@ if(isset($_GET['addFilmStaff'])) {
     $lastId = 1;
     $time_pre = microtime(true);
     for ($i = 0; $i < $_GET["addFilmStaff"]; $i++) {
-        $sql = "SELECT MAX(kinopoiskId) FROM staff";
+        $sql = "SELECT MAX(personId) FROM staff";
         $result = mysqli_query($conn, $sql);
         if ($result) {
             while ($row = mysqli_fetch_row($result)) {
@@ -100,12 +102,12 @@ if(isset($_GET['addFilmStaff'])) {
             }
             mysqli_free_result($result);
         }
-        $amount_ids = 100;
+        $amount_ids = 500;
         echo "+++starting job for staff with lastId=" . $lastId . ";step=" . $amount_ids . ";+++<br>";
-        $lastId = addStaff($lastId, $amount_ids, $db, $conn);
+        $newlastId = addStaff($lastId, $amount_ids, $db, $conn);
         $time_post = microtime(true);
         $exec_time = $time_post - $time_pre;
-        echo "<br>+++end job with " . $lastId . " new lines in " . sprintf('%f', $exec_time) . " +++<br>";
+        echo "<br>+++end job with " . $newlastId . " new lines in " . sprintf('%f', $exec_time) . " +++<br>";
     }
 }
 
@@ -117,17 +119,17 @@ function addStaff($startId,$amount_ids,$db,$conn){
         'timeout'  => 2.0,
         'headers' => ['X-API-KEY' => 'da67006f-9505-4e51-a1ab-eb100c711635']
     ]);
-    $kinopoiskIds = array();
-    $sql = "SELECT kinopoiskId FROM films where kinopoiskId between ".$startId." and ".$amount_ids;
+    $personIds = array();
+    $sql = "SELECT personId FROM staff where persondId between ".$startId." and ".$amount_ids;
     $result = mysqli_query($conn, $sql);
     if ($result) {
         while ($row = mysqli_fetch_row($result)) {
-            array_push($kinopoiskIds, $row[0]);
+            array_push($personIds, $row[0]);
         }
     }
     $finished=false;
     for ($i = $startId+1; $i < $startId+$amount_ids; $i++) {
-        if (!in_array($i, $kinopoiskIds)) {
+        if (!in_array($i, $personIds)) {
             try {
                 $response = $client->request('GET', (string)$i);
                 if ($response->getStatusCode() == 200) {
@@ -140,7 +142,7 @@ function addStaff($startId,$amount_ids,$db,$conn){
         if($i===$startId+$amount_ids-1){$finished=true;}
     }
     $new_last_id=0;
-    $sql = "SELECT MAX(kinopoiskId) FROM films";
+    $sql = "SELECT MAX(personId) FROM staff";
     $result = mysqli_query($conn, $sql);
     if ($result) {
         while ($row = mysqli_fetch_row($result)) {
